41d0e51c9be045738c3dd3e949f5e8f0f917a7e7
Adi Malca <adimalca91@gmail.com>
2024-02-01 09:08:20 +0200

    Added DetailView with its default template

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.taskList, name='task-list'),
     path('', views.TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
@@ -11,3 +12,8 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'    # Instead of the default 'object_list'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    

9cb213644e0561f6cb392b8a544881e65613a430
Adi Malca <adimalca91@gmail.com>
2024-02-01 18:33:00 +0200

    Fixed a return in RegisterPage and added some style

--- base/views.py
+++ base/views.py
@@ -41,7 +41,7 @@ class RegisterPage(FormView):
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect('tasks')
-        return(RegisterPage, self).get(*args, **kwargs)
+        return super(RegisterPage, self).get(*args, **kwargs)
         
     
     
